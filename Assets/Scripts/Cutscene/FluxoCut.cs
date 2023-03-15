5439e686250df65b47a8fe3b26bb3e1d13492fba
Laryssa Maciel <maciel.f.laryssa@gmail.com>
2023-03-14 21:02:08 -0300

    feature split camera no inicio

--- Assets/Scripts/Cutscene/FluxoCut.cs
+++ Assets/Scripts/Cutscene/FluxoCut.cs
@@ -7,6 +7,8 @@ public class FluxoCut : MonoBehaviour
     private Controles controles;
     private Vector2 inputs;
 
+    public GameObject p2cam, p1cam;
+
     void Awake()
     {
         controles = new Controles();
@@ -17,6 +19,8 @@ public class FluxoCut : MonoBehaviour
     {
         if (col.CompareTag("P1") && controles.P1.Interagir.ReadValue<float>() == 1)
         {
+            p2cam.SetActive(true);
+            p1cam.GetComponent<Camera>().rect = new Rect(0, 0, 0.5f, 1);
             Destroy(this.gameObject);
         }
     }
