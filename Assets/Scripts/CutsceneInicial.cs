62b075f377d9c1e5191ba36dfa3b419f655f9b4d
Laryssa Maciel <maciel.f.laryssa@gmail.com>
2023-04-02 17:05:20 -0300

    base da feature do sufocamento do p2

--- Assets/Scripts/CutsceneInicial.cs
+++ Assets/Scripts/CutsceneInicial.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 
@@ -13,6 +14,9 @@ public class CutsceneInicial : MonoBehaviour
     public string[] textos;
     public TextMeshProUGUI text_;
     public int count = 0;
+    
+    public float timer = 60f, curTime, timerStart = 0;
+    public GameObject portaP2, painel, sufocado;
 
     void Awake()
     {
@@ -26,22 +30,49 @@ public class CutsceneInicial : MonoBehaviour
             gameObject.transform.GetChild(0).gameObject.SetActive(true);
             PlayerPrefs.SetString("ComeçouJogar", "false");
         }
+
+        curTime = timer;
     }
 
     void Update()
     {
-        text_.text = textos[count];
-        
         if (controles.Any.AnyKey.triggered)
             count++;
         
-        if (count == textos.Length)
+        if (count > textos.Length)
+        {
+            if (!cinematica.activeSelf && timerStart == 1)
+            {
+                timerStart = 2;
+            }
+        }
+        else if (count == textos.Length)
         {
             PlayerPrefs.SetString("ComeçouJogar", "true");
             PlayerPrefs.SetString("podeAndar", "true");
             PlayerPrefs.SetString("ShowBtnInfo1", "true");
             if (cinematica != null) { cinematica.SetActive(true); }
-            Destroy(this.gameObject);
+            timerStart = 1;
+            Destroy(painel);
+            sufocado.SetActive(true);
+        }
+        else if (count < textos.Length)
+        {
+            text_.text = textos[count];
+        }
+
+        if (timerStart == 2)
+        {
+            curTime -= Time.deltaTime;
+            sufocado.GetComponent<Image>().color += new Color(0f, 0f, 0f, 0.0001f); 
+        }
+
+        if (curTime <= 0)
+            SceneManager.LoadScene("GameOver");
+        else
+        {
+            if (portaP2.GetComponent<OpenDoon>().unlocked == true)
+                Destroy(this.gameObject);
         }
     }
 }
