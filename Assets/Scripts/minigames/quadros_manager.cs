7b82c6e0caf7f3e13505c877a6bd93f281df2edf
Laryssa Maciel <maciel.f.laryssa@gmail.com>
2023-05-02 23:00:14 -0300

    update minigame quadros

--- Assets/Scripts/minigames/quadros_manager.cs
+++ Assets/Scripts/minigames/quadros_manager.cs
@@ -11,42 +11,21 @@ public class quadros_manager : MonoBehaviour
 
     void Update()
     {
-        if ( (p1quadros[0].GetComponent<Image>().color == cores_[0] && p2quadros[0].GetComponent<Image>().color == cores_[2]) 
+        Manager(2, 1, 2, new Vector3(.5f, 0, 1));
+        Manager(1, 1, 0, new Vector3(1, 0.64f, 0));
+        Manager(0, 2, 0, new Vector3(0, 1, 0));
+    }
+
+    void Manager(int p, int cor1, int cor2, Vector3 cor)
+    {
+        if ( (p1quadros[p].GetComponent<Image>().color == cores_[cor1] && p2quadros[p].GetComponent<Image>().color == cores_[cor2]) 
             ||
-            (p1quadros[0].GetComponent<Image>().color == cores_[2] &&
-            p2quadros[0].GetComponent<Image>().color == cores_[0]) ) 
+            (p1quadros[p].GetComponent<Image>().color == cores_[cor2] && p2quadros[p].GetComponent<Image>().color == cores_[cor1]) )
         {
-            print ("verde");
-            p1quadros[0].GetComponent<Image>().color = new Color(0, 1, 0);
-            p2quadros[0].GetComponent<Image>().color = new Color(0, 1, 0);
-            p1quadros[0].GetComponent<Button>().interactable = false;
-            p2quadros[0].GetComponent<Button>().interactable = false;
-        }
-        
-        if ( (p1quadros[1].GetComponent<Image>().color == cores_[0] &&
-            p2quadros[1].GetComponent<Image>().color == cores_[1]) 
-            ||
-            (p1quadros[1].GetComponent<Image>().color == cores_[1] &&
-            p2quadros[1].GetComponent<Image>().color == cores_[0]) )
-        {
-            print ("laranja");
-            p1quadros[1].GetComponent<Image>().color = new Color(1.0f, 0.64f, 0.0f);
-            p2quadros[1].GetComponent<Image>().color = new Color(1.0f, 0.64f, 0.0f);
-            p1quadros[1].GetComponent<Button>().interactable = false;
-            p2quadros[1].GetComponent<Button>().interactable = false;
-        }
-        
-        if ( (p1quadros[2].GetComponent<Image>().color == cores_[1] &&
-            p2quadros[2].GetComponent<Image>().color == cores_[2]) 
-            ||
-            (p1quadros[2].GetComponent<Image>().color == cores_[2] &&
-            p2quadros[2].GetComponent<Image>().color == cores_[1]) )
-        {
-            print ("roxo");
-            p1quadros[2].GetComponent<Image>().color = new Color(.5f, 0f, 1f);
-            p2quadros[2].GetComponent<Image>().color = new Color(.5f, 0f, 1f);
-            p1quadros[2].GetComponent<Button>().interactable = false;
-            p2quadros[2].GetComponent<Button>().interactable = false;
+            p1quadros[p].GetComponent<Image>().color = new Color(cor.x, cor.y, cor.z);
+            p2quadros[p].GetComponent<Image>().color = new Color(cor.x, cor.y, cor.z);
+            p1quadros[p].GetComponent<Button>().interactable = false;
+            p2quadros[p].GetComponent<Button>().interactable = false;
         } 
     }
 }
