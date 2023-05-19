e52a900caecb5b257b8d97fcdae213d791b922f5
Laryssa Maciel <maciel.f.laryssa@gmail.com>
2023-05-19 08:48:52 -0300

    sistem de skip cutscene e ajustes menores

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
@@ -37,7 +36,7 @@ public class PlayerController : MonoBehaviour
                 hasLockp = false,
                 hasArma = false;
     
-    public GameObject canvasMenu, inimigo, falap1, falap2;
+    public GameObject inimigo, falap1, falap2;
 
     private AudioSource audio_, audio_1;
 
@@ -73,7 +72,6 @@ public class PlayerController : MonoBehaviour
     {
         Gravity();
         Animations();
-        ShowMenu();
 
         if (PlayerPrefs.GetString("podeAndar") == "true")
         {
@@ -255,43 +253,6 @@ public class PlayerController : MonoBehaviour
             audio_.enabled = false;
             audio_1.enabled = false;
         }
-    }
-
-    public bool inv = false, open = false;
-    void ShowMenu()
-    {
-        if (controles.P1.Menu.triggered) { inv = !inv; }
-
-        if (inv == true)
-        { 
-            this.canvasMenu.SetActive(true);
-            if (open == false) 
-            {
-                EventSystem.current.SetSelectedGameObject(canvasMenu.transform.GetChild(1).transform.GetChild(0).gameObject);
-                open = true;
-            }
-        }
-
-        if (inv == false)
-        { 
-            canvasMenu.transform.GetChild(1).gameObject.SetActive(true);
-            canvasMenu.transform.GetChild(1).transform.GetChild(0).gameObject.SetActive(true);
-            canvasMenu.transform.GetChild(1).transform.GetChild(1).gameObject.SetActive(true);
-            canvasMenu.transform.GetChild(1).transform.GetChild(2).gameObject.SetActive(true);
-            canvasMenu.transform.GetChild(2).gameObject.SetActive(false);
-            canvasMenu.transform.GetChild(3).gameObject.SetActive(false);
-            canvasMenu.transform.GetChild(4).gameObject.SetActive(false);
-            this.canvasMenu.SetActive(false); 
-            open = false;
-        }
-
-        if (canvasMenu.activeSelf) { Time.timeScale = 0; } // PlayerPrefs.SetString("podeAndar", "false"); }
-        else { Time.timeScale = 1; } // PlayerPrefs.SetString("podeAndar", "true"); }
-    }
-
-    public void CloseMenu()
-    {
-        inv = false;
     }
 
     // void ShowCursor()
