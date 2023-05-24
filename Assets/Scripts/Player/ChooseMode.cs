95faeb6dfb97fe0564feb22fda5d5d7b368a7116
Laryssa Maciel <maciel.f.laryssa@gmail.com>
2023-05-24 11:54:50 -0300

    troca de movimentação salva no menu de dropdown

--- Assets/Scripts/Player/ChooseMode.cs
+++ Assets/Scripts/Player/ChooseMode.cs
@@ -1,36 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class ChooseMode : MonoBehaviour
 {
     public string texto;
+    private int valor;
 
     void Awake()
     {
         texto = this.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text;
         if (this.gameObject.name.Contains("P1") && PlayerPrefs.HasKey("P1ModoMov"))
-        {
-            texto = PlayerPrefs.GetString("P1ModoMov");
-        }
+            { this.texto = PlayerPrefs.GetString("P1ModoMov"); }
         if (this.gameObject.name.Contains("P2") && PlayerPrefs.HasKey("P2ModoMov"))
-        {
-            texto = PlayerPrefs.GetString("P2ModoMov");
-        }
+            { this.texto = PlayerPrefs.GetString("P2ModoMov"); }
     }
+
+    void Start() => this.GetComponent<TMPro.TMP_Dropdown>().captionText.text = this.texto;
 
     public void Mode(TextMeshProUGUI text)
     {
         if (this.gameObject.name.Contains("P1"))
-        {
             PlayerPrefs.SetString("P1ModoMov", text.text);
-            print(PlayerPrefs.GetString("P1ModoMov"));
-        }
         else if (this.gameObject.name.Contains("P2"))
-        {
             PlayerPrefs.SetString("P2ModoMov", text.text);
-            print(PlayerPrefs.GetString("P2ModoMov"));
-        }
     }
 }
